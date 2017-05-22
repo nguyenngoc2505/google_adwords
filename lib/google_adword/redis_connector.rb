88b4e6988963385f88aa1f2d8741dd19dbd1e8e6
Ngoc Nguyen <nguyenngoc2505@gmail.com>
2017-05-22 14:18:55 +0700

    Add redis config for heroku

--- lib/google_adword/redis_connector.rb
+++ lib/google_adword/redis_connector.rb
@@ -1,9 +1,14 @@
 module GoogleAdword
-  class RedisConnector < Redis
+  class RedisConnector < ::Redis
     REDIS_CONFIG = YAML.load_file(::Rails.root.join "config/database.yml")
 
     def initialize db = 0
-      super host: REDIS_CONFIG[::Rails.env]["redis"]["host"], db: db
+      options = if ::Rails.env.development?
+        {host: REDIS_CONFIG[::Rails.env]["redis"]["host"], db: db}
+      else
+        {db: db}
+      end
+      super options
     end
   end
 end
